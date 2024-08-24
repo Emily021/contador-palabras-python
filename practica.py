1517bee180f0a75b576a1ee97ad3a92bd4ca0348
Emily <arce.emily.21@gmail.com>
2024-08-24 12:58:47 -0300

    Realize un contador de palabras para una tarea facultativa de diagrama de flujos

---
+++ practica.py
@@ -0,0 +1,22 @@
+
+
+#paso 1: Solicitar al usuario que ingrese una palabra 
+#   Mostrar mensaje: // paso 1: Solicitar al usuario que ingrese una palabra
+#   definir varible palabra_ingresada con la lectura de la palabra 
+
+palabra_ingresada= input(" Por favor, ingresa una palabra: ")
+
+
+
+#    // paso 2: Contar la cantidad de letras de la palabra ingresada.
+#    Definir la variable cantidad_letras y asignarle la  longitud de palabra_ingresada
+
+
+cant_letras = len(palabra_ingresada)
+
+print("La palabra ingresada tiene", cant_letras, "letras.")
+
+
+
+#   // paso 3: Mostrar al usuario la cantidad de letras que tiene la palabra ingresada 
+#   Mostrar mensaje: "la palabra ingresada tiene", cantidad_letras, "letras."
